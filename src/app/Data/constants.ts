a7169d1ff61b362d820c74510c0a8dd705011465
Omarelshazly14 <omarelshazly014@gmail.com>
2022-12-13 04:45:07 +0200

    changing apiroot

--- src/app/Data/constants.ts
+++ src/app/Data/constants.ts
@@ -1,6 +1,6 @@
 export class Constants {
-  // public static apiRoot = "http://196.218.181.250:1313/";
-  public static apiRoot = "http://localhost:5207";
+  public static apiRoot = "http://196.218.181.250:1313";
+  // public static apiRoot = "http://localhost:5207";
   public static clientRoot = "http://localhost:4200";
   public static idpAuthority = "https://localhost:44310"
   public static clientId = "HR.web";
